322dec9d474a756c07ba8333d814bdc96c4d0521
Fredrick Kelly <38326256+iamfredrickokereke@users.noreply.github.com>
2018-07-01 12:14:56 +0100

    Create design.js

---
+++ design.js
@@ -0,0 +1,44 @@
+// Select color input
+
+function startColor() {
+  $("td").click(function () {
+  const color = $("#colorPicker").val();
+  if ($(this).attr("style")) {
+      $(this).removeAttr("style");
+   } else {
+      $(this).attr("style", "background-color:" + color);
+   }
+  }
+ );
+}
+// Select size input
+
+$("#sizePicker").submit(function (evt) {
+  evt.preventDefault();
+  let height = $("#inputHeight").val();
+  let width = $("#inputWeight").val();
+  makeGrid(height, width);
+  startColor();
+  console.log(`the height is ${height} and the width is ${width}`);
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(row, col) {
+
+  $("tr").remove();
+
+let x=1;
+while(x <= row){
+ $("#pixelCanvas").append("<tr></tr>");
+  x++;
+  }
+  let y=1;
+while(y <= col){
+
+$("tr").append("<td></td>");
+  
+  y++;}
+  
+}
+
